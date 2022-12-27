b481c27e2ccba70921a66f2b84a7eaad1f2dbbf9
rivenbryan <rivenbryan@gmail.com>
2022-12-27 16:37:09 +0800

    ok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import './style.css'
 
-function App() {
+const App = () => {
+
+  const [formValue, setFormValue] = React.useState({
+    email: "",
+    password: "",
+    confirmpassword: "",
+    isJoined: false
+
+  })
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const successString = formValue.password === formValue.confirmpassword ? "Successfully signed up!" : "Password do not match!"
+    const newsLetterString = formValue.isJoined ? "Thank you for subscribing!" : ""
+
+    console.log(successString)
+
+    if(formValue.password === formValue.confirmpassword && newsLetterString) {
+      console.log(newsLetterString)
+    }
+  }
+
+  const handleEmail = (event) => {
+    setFormValue(prevFormValue => {
+      return {
+        ...prevFormValue,
+        email: event.target.value
+      }
+    })
+  }
+
+  const handlePW = (event) => {
+    const { name, value } = event.target
+    setFormValue(prevFormValue => {
+
+      if (name === 'pw') {
+        return {
+          ...prevFormValue,
+          password: value
+        }
+      } else if (name === 'confirmpw') {
+        return {
+          ...prevFormValue,
+          confirmpassword: value
+        }
+      }
+    })
+
+  }
+
+  const handleNews = () => {
+    setFormValue(prevFormValue => {
+      return {
+        ...prevFormValue,
+        isJoined: !prevFormValue.isJoined
+      }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="form-container">
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email address"
+          className="form--input"
+          onChange={handleEmail}
+          value={formValue.email}
+        />
+        <input
+          name="pw"
+          type="password"
+          placeholder="Password"
+          className="form--input"
+          onChange={handlePW}
+          value={formValue.password}
+        />
+        <input
+          name="confirmpw"
+          type="password"
+          placeholder="Confirm password"
+          className="form--input"
+          onChange={handlePW}
+          value={formValue.confirmpassword}
+        />
+
+        <div className="form--marketing">
+          <input
+            id="okayToEmail"
+            type="checkbox"
+            name="joinNews"
+            onChange={handleNews}
+
+
+          />
+          <label htmlFor="okayToEmail">I want to join the newsletter</label>
+        </div>
+        <button
+          className="form--submit"
         >
-          Learn React
-        </a>
-      </header>
+          Sign up
+        </button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App 
